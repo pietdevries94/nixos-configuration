66b82d814802d4c8e899c525f814948373f8fa18
pietdevries94 <piet@devries.tech>
2020-07-27 22:59:10 +0200

    Add history substring search to zsh

--- home-manager/piet/base.nix
+++ home-manager/piet/base.nix
@@ -9,7 +9,20 @@
     enableCompletion = true;
     initExtra = ''
       zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+      bindkey "$terminfo[kcuu1]" history-substring-search-up
+      bindkey "$terminfo[kcud1]" history-substring-search-down
     '';
+    plugins = [
+      {
+        name = "zsh-history-substring-search";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-history-substring-search";
+          rev = "v1.0.2";
+          sha256 = "0y8va5kc2ram38hbk2cibkk64ffrabfv1sh4xm7pjspsba9n5p1y";
+        };
+      }
+    ];
   };
 
   programs.starship = {
