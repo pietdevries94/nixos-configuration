5b0fb2825603113edd972c4a14c7732fa495e7c1
pietdevries94 <piet@devries.tech>
2020-09-02 20:58:39 +0200

    Add light control

--- machines/base.nix
+++ machines/base.nix
@@ -13,11 +13,15 @@
   system.stateVersion = "20.03";
   system.autoUpgrade.enable = true;
 
+  # needed for ddcutil
+  boot.kernelModules = [ "i2c-dev" ];
+
   environment.systemPackages = with pkgs; [
     htop
     killall
     cachix
     lm_sensors
+    ddcutil
   ];
 
   # This binary cache is for my personal repository https://github.com/pietdevries94/personal-nix-channel
@@ -31,10 +35,21 @@
   i18n.defaultLocale = "en_US.UTF-8";
   time.timeZone = "Europe/Amsterdam";
 
-  programs.zsh.enable = true;
+  programs = {
+    zsh.enable = true;
+  };
+
   users.users.piet = {
     shell = pkgs.zsh;
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
+    extraGroups = [ "wheel" "video" "networkmanager" ];
   };
+
+  security.sudo.enable = true;
+  security.sudo.extraConfig = ''
+    %wheel      ALL=(ALL:ALL) NOPASSWD: ${pkgs.systemd}/bin/poweroff
+    %wheel      ALL=(ALL:ALL) NOPASSWD: ${pkgs.systemd}/bin/reboot
+    %wheel      ALL=(ALL:ALL) NOPASSWD: ${pkgs.systemd}/bin/systemctl suspend
+    %wheel      ALL=(ALL:ALL) NOPASSWD: ${pkgs.ddcutil}/bin/ddcutil
+  '';
 }
