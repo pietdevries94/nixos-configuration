af31b3479b425d24531805f670172fb590fc639f
Piet de Vries <piet@compenda.nl>
2021-01-02 17:33:02 +0100

    Brightness controls added

--- machines/nixos-work-laptop/configuration.nix
+++ machines/nixos-work-laptop/configuration.nix
@@ -59,16 +59,10 @@ in {
     };
   };
 
-
+  # laptop specific settings
   services.xserver.libinput.enable = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.09"; # Did you read the comment?
-
+  programs.light.enable = true;
+  home-manager.users.piet.services.sxhkd.keybindings = {
+    "XF86MonBrightness{Down,Up}" = "light {-U,-A} 10";
+  };
 }
-
