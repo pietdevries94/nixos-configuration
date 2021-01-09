bf403fe9939bc6e590aff21cc24721c102c3ca08
Piet de Vries <piet@compenda.nl>
2021-01-09 13:47:33 +0100

    Disable virtualbox extension pack

--- services/virtualbox.nix
+++ services/virtualbox.nix
@@ -3,7 +3,9 @@
 {
   virtualisation.virtualbox.host = {
     enable = true;
-    enableExtensionPack = true;
+    
+    # Enabling this triggers frequent recompiles and is only needed for passtrough, which I don't need
+    # enableExtensionPack = true;
   };
   users.extraGroups.vboxusers.members = [ "piet" ];
 }
