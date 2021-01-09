22c44229292c6c0c1d4790501abc7cc2649a922f
pietdevries94 <piet@devries.tech>
2021-01-09 15:34:29 +0100

    Cleanup lorri file

--- home-manager/piet/terminal/lorri.nix
+++ home-manager/piet/terminal/lorri.nix
@@ -6,31 +6,4 @@
     enable = true;
     enableZshIntegration = true;
   };
-
-  programs.zsh = {
-    enable = true;
-    enableAutosuggestions = true;
-    enableCompletion = true;
-    initExtra = ''
-      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-      bindkey "$terminfo[kcuu1]" history-substring-search-up
-      bindkey "$terminfo[kcud1]" history-substring-search-down
-    '';
-    plugins = [
-      {
-        name = "zsh-history-substring-search";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-history-substring-search";
-          rev = "v1.0.2";
-          sha256 = "0y8va5kc2ram38hbk2cibkk64ffrabfv1sh4xm7pjspsba9n5p1y";
-        };
-      }
-    ];
-  };
-
-  programs.starship = {
-    enable = true;
-    enableZshIntegration = true;
-  };
 }
