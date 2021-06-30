979b366738c359dda8c2438118cdd24e8f13b34a
pietdevries94 <piet@devries.tech>
2021-06-30 12:13:11 +0200

    Add notification gestures

--- home-manager/piet/xsession/libinput-gestures.nix
+++ home-manager/piet/xsession/libinput-gestures.nix
@@ -11,6 +11,10 @@ gesture swipe down	3 xdotool key Escape
 gesture swipe left	3 xdotool key alt+Right
 gesture swipe right	3 xdotool key alt+Left
 
+# Notifications
+gesture swipe up	4 xdotool key ctrl+space
+gesture swipe down	4 xdotool key ctrl+Escape
+
 # Zoom in and out
 gesture pinch in	xdotool key ctrl+equal
 gesture pinch out	xdotool key ctrl+minus
