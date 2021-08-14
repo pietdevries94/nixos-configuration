75b3e39eda5a2356554312807ed04221bf0a7579
pietdevries94 <piet@devries.tech>
2021-08-14 09:52:20 +0200

    Migrate most of base to minimal setup

--- machines/deprecated-base.nix
+++ machines/deprecated-base.nix
@@ -3,57 +3,6 @@
 { config, pkgs, options, lib, ... }:
 
 {
-  nixpkgs = {
-    config.allowUnfree = true;
-    overlays = [
-      (import ../overlays/unstable.nix)
-      (import ../overlays/personal.nix)
-      (import ../overlays/wietsedv.nix)
-      (import ../overlays/extra-vscode-extensions.nix)
-      (import ../overlays/pull-requests.nix)
-    ];
-  };
-
-  system.stateVersion = "21.05";
-  system.autoUpgrade.enable = true;
-
-  # needed for ddcutil
-  boot.kernelModules = [ "i2c-dev" ];
-
-  environment.systemPackages = with pkgs; [
-    htop
-    killall
-    cachix
-    lm_sensors
-    ddcutil
-  ];
-
-  # This binary cache is for my personal repository https://github.com/pietdevries94/personal-nix-channel
-  nix = {
-    binaryCaches = [ "https://pietdevries94.cachix.org" ];
-    binaryCachePublicKeys = [ "pietdevries94.cachix.org-1:EmL0bay0YH7tI9SU3sLeyhBR5QYx+Zg6vMvBOmZ6MMQ=" ];
-
-    # Support flakes
-    package = pkgs.nixFlakes;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-
-  boot.cleanTmpDir = true;
-  
-  i18n.defaultLocale = "en_US.UTF-8";
-  time.timeZone = "Europe/Amsterdam";
-  networking.networkmanager.enable = true;
-
-  programs = {
-    zsh.enable = true;
-  };
-
-  services.gvfs = {
-    enable = true;
-  };
-
   networking.firewall.allowedTCPPortRanges = [
     # KDE Connect
     {
@@ -61,21 +10,4 @@
       to = 1764;
     }
   ];
-
-  users.users.piet = {
-    # Password made with mkpasswd -m sha-512
-    hashedPassword = import ../secrets/password-piet.nix;
-
-    shell = pkgs.zsh;
-    isNormalUser = true;
-    extraGroups = [ "wheel" "video" "networkmanager" "input" ];
-  };
-
-  security.sudo.enable = true;
-  security.sudo.extraConfig = ''
-    %wheel      ALL=(ALL:ALL) NOPASSWD: ${pkgs.systemd}/bin/poweroff
-    %wheel      ALL=(ALL:ALL) NOPASSWD: ${pkgs.systemd}/bin/reboot
-    %wheel      ALL=(ALL:ALL) NOPASSWD: ${pkgs.systemd}/bin/systemctl suspend
-    %wheel      ALL=(ALL:ALL) NOPASSWD: ${pkgs.ddcutil}/bin/ddcutil
-  '';
 }
--- templates/minimal.nix
+++ templates/minimal.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 let
   hmTarball = fetchTarball https://github.com/nix-community/home-manager/archive/release-20.09.tar.gz;
@@ -7,6 +7,74 @@ in {
     ../custom-modules
     (import "${hmTarball}/nixos")
   ];
+
+    nixpkgs = {
+    config.allowUnfree = true;
+    overlays = [
+      (import ../overlays/unstable.nix)
+      (import ../overlays/personal.nix)
+      (import ../overlays/wietsedv.nix)
+      (import ../overlays/extra-vscode-extensions.nix)
+      (import ../overlays/pull-requests.nix)
+    ];
+  };
+
+  system.stateVersion = "21.05";
+  system.autoUpgrade.enable = true;
+
+  # needed for ddcutil
+  boot.kernelModules = [ "i2c-dev" ];
+
+  environment.systemPackages = with pkgs; [
+    htop
+    killall
+    cachix
+    lm_sensors
+    ddcutil
+  ];
+
+  # This binary cache is for my personal repository https://github.com/pietdevries94/personal-nix-channel
+  nix = {
+    binaryCaches = [ "https://pietdevries94.cachix.org" ];
+    binaryCachePublicKeys = [ "pietdevries94.cachix.org-1:EmL0bay0YH7tI9SU3sLeyhBR5QYx+Zg6vMvBOmZ6MMQ=" ];
+
+    # Support flakes
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  boot.cleanTmpDir = true;
+  
+  i18n.defaultLocale = "en_US.UTF-8";
+  time.timeZone = "Europe/Amsterdam";
+  networking.networkmanager.enable = true;
+
+  programs = {
+    zsh.enable = true;
+  };
+
+  services.gvfs = {
+    enable = true;
+  };
+
+  users.users.piet = {
+    # Password made with mkpasswd -m sha-512
+    hashedPassword = import ../secrets/password-piet.nix;
+
+    shell = pkgs.zsh;
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" "networkmanager" "input" ];
+  };
+
+  security.sudo.enable = true;
+  security.sudo.extraConfig = ''
+    %wheel      ALL=(ALL:ALL) NOPASSWD: ${pkgs.systemd}/bin/poweroff
+    %wheel      ALL=(ALL:ALL) NOPASSWD: ${pkgs.systemd}/bin/reboot
+    %wheel      ALL=(ALL:ALL) NOPASSWD: ${pkgs.systemd}/bin/systemctl suspend
+    %wheel      ALL=(ALL:ALL) NOPASSWD: ${pkgs.ddcutil}/bin/ddcutil
+  '';
 
   home-manager.useUserPackages = true;
   home-manager.useGlobalPkgs = true;
