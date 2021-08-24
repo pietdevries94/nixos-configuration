b15f9c5dd0715d4f281ad1b4847df865fa48bbdd
Piet de Vries <piet@compenda.nl>
2021-08-24 10:40:15 +0200

    Show rofi icons

--- home-manager/piet/xsession/rofi.nix
+++ home-manager/piet/xsession/rofi.nix
@@ -33,10 +33,11 @@
         };
       };
     };
+    scrollbar = false;
   };
 
   services.sxhkd.keybindings = {
-    "mod1 + space" = "${pkgs.rofi}/bin/rofi -show drun -modi drun -display-drun \"Search\"";
-    "mod1 + Tab" = "${pkgs.rofi}/bin/rofi -show window -modi window -display-window \"Window\" -me-accept-entry \"MousePrimary\" -me-select-entry \"MouseDPrimary\"";
+    "mod1 + space" = "${pkgs.rofi}/bin/rofi -show drun -modi drun -display-drun \"Search\" -show-icons";
+    "mod1 + Tab" = "${pkgs.rofi}/bin/rofi -show window -modi window -display-window \"Window\" -me-accept-entry \"MousePrimary\" -me-select-entry \"MouseDPrimary\" -show-icons";
   };
 }
