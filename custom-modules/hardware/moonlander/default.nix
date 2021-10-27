20a4f72850ac12cb7788e0895bb961452d8a4aae
Piet de Vries <piet@compenda.nl>
2021-10-27 15:34:57 +0200

    wip

--- custom-modules/hardware/moonlander/default.nix
+++ custom-modules/hardware/moonlander/default.nix
@@ -2,6 +2,9 @@
 with lib;
 let
   cfg = config.custom.hardware.moonlander;
+  foo = pkgs.writeScript "foo.sh" ''#!${pkgs.bash}/bin/bash
+  echo 'cat' >> /tmp/foo
+  '';
 in {
   options.custom.hardware.moonlander = {
     enable = mkEnableOption "Moonlander";
@@ -13,6 +16,8 @@ in {
       SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", \
         MODE:="0666", \
         SYMLINK+="stm32_dfu"
+
+      ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="3297", ATTR{idProduct}=="1969", RUN+="${foo}"
     '';
     users = {
       groups.plugdev = {};
